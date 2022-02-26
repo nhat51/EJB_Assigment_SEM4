7731f820705e0f398c008d4d95a9968ddab6f0af
nhat51 <65001571+nhat51@users.noreply.github.com>
2022-02-26 20:26:49 +0700

    setup project

---
+++ EmyBank/src/test/java/com/example/emybank/EmyBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmyBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
