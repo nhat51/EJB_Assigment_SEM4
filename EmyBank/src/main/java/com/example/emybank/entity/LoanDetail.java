359c12d1da0cc5d441b22399c50b3b32acabc026
nhat51 <65001571+nhat51@users.noreply.github.com>
2022-02-28 22:37:24 +0700

    created entity
    merge luôn branch vào master nhé để mọi người lấy về làm

---
+++ EmyBank/src/main/java/com/example/emybank/entity/LoanDetail.java
@@ -0,0 +1,29 @@
+package com.example.emybank.entity;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "loan_details")
+public class LoanDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int loan_id;
+    private int user_id;
+    private LocalDate created_at;
+    private double amount;
+    private double interest_rate;
+    private int tenor; // thời hạn vay
+    private String status;
+}
---
+++ EmyBank/src/main/java/com/example/emybank/entity/TransactionHistory.java
@@ -0,0 +1,36 @@
+package com.example.emybank.entity;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "transaction_histories")
+public class TransactionHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int transactionId;
+    @Column(name = "sender_id",insertable = false,updatable = false)
+    private int sender_id;
+    private int receiver_id;
+    private double amount;
+    private String message;
+    private LocalDate created_at;
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    @JsonIgnore
+    User user;
+}
