7c1be79eaaa4b1dcee129ac9fa0f2d78d0420f52
nhat51 <65001571+nhat51@users.noreply.github.com>
2022-03-07 12:13:17 +0700

    update security config

--- EmyBank/src/main/java/com/example/emybank/config/MySecurityConfig.java
+++ EmyBank/src/main/java/com/example/emybank/config/MySecurityConfig.java
@@ -5,15 +5,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -37,8 +42,14 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         ApiAuthenticationFilter apiAuthenticationFilter = new ApiAuthenticationFilter(authenticationManagerBean());
         apiAuthenticationFilter.setFilterProcessesUrl("/api/v1/accounts/login");
+        http.cors().configurationSource(request -> {
+            CorsConfiguration cors = new CorsConfiguration();
+            cors.setAllowedOrigins(Collections.singletonList("*"));
+            cors.setAllowedMethods(Collections.singletonList("*"));
+            cors.setAllowedHeaders(Collections.singletonList("*"));
+            return cors;
+        });
         http.csrf().disable();
-        http.cors().disable();
         http.authorizeHttpRequests()
                 .antMatchers("/api/v1/accounts/register", "/api/v1/accounts/login").permitAll()
                 .anyRequest().authenticated()
